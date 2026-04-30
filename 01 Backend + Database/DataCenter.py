9e68818b393fa5e12a39eae4020ee55272a8515c
TylerM <tytym502@gmail.com>
2026-04-29 21:04:25 -0500

    Backend + Database: Phase 2, Step 7 Complete

--- 01 Backend + Database/DataCenter.py
+++ 01 Backend + Database/DataCenter.py
@@ -25,6 +25,11 @@ class Category:
     warning_threshold_percent: int = 80
     items: List[Item] = field(default_factory = list)
 
+@dataclass
+class Storage:
+    location: str
+    items: List[Item] = field(default_factory=list)
+
 class Inventory:
     def __init__(self, db_path='inventory.db'):
         # database path
@@ -33,6 +38,10 @@ class Inventory:
         self.conn = sqlite3.connect(db_path, check_same_thread= False)
         # create tables
         self.create_tables()
+        # variables to access database
+        self.categories: Dict[str, Category] = {}
+        self.storages: Dict[str, Storage] = {}
+        self.items: Dict[int, Item] = {}
         # load existing data
         self.load_data()
 
@@ -62,10 +71,64 @@ class Inventory:
         self.conn.commit()
 
     def load_data(self):
-        # Placeholder for Part 2
-        pass
+        self.categories = {}
+        self.storages = {}
+        self.items = {}
 
+        # load categories
+        cursor = self.conn.execute('SELECT * FROM categories')
+        # go through database rows
+        for row in cursor:
+            cat = Category(
+                name=row[0],
+                storage_location=row[1],
+                recommended_exp_days=row[2],
+                warning_threshold_percent=row[3]
+            )
+            self.categories[cat.name.lower()] = cat
+
+        # load non archived items
+        cursor = self.conn.execute('SELECT * FROM items WHERE archived = 0')
+        for row in cursor:
+            exp_date = datetime.fromisoformat(row[5])
+            purchase_date = datetime.fromisoformat(row[6])
+            # skip archived in row 7 because it is used above
+            archived_date = datetime.fromisoformat(row[8]) if row[8] else None
+
+            item = Item(
+                item_id=row[0],
+                barcode=row[1],
+                name=row[2],
+                category=row[3],
+                storage_location=row[4],
+                exp_date=exp_date,
+                purchase_date=purchase_date,
+                archived=bool(row[7]),
+                archived_date=archived_date
+            )
+
+            self.items[item.item_id] = item
+            
+            cat_key = item.category.lower()
+            if cat_key not in self.categories:
+                self.categories[cat_key] = Category(
+                    name=item.category,
+                    storage_location=item.storage_location
+                )
+            self.categories[cat_key].items.append(item)
+
+            storage_key = item.storage_location.lower()
+            if storage_key not in self.storages:
+                self.storages[storage_key] = Storage(location=item.storage_location)
+
+            self.storages[storage_key].items.append(item)
     def close(self):
         """Close the database connection"""
         if self.conn:
             self.conn.close()
+
+
+inventory = Inventory()
+print(inventory.categories)
+print(inventory.items)
+print(inventory.storages)
